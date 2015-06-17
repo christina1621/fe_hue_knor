0bf3013274c646c895a59f2d19eda75d92d2ad9f
christina <s1310456018@students.fh-hagenberg.at>
2015-06-17 11:27:03 +0200

    commit message

---
+++ Desktop/Frontend/01_christina_knor/src_exercise/css/app.cs
@@ -0,0 +1,132 @@
+html {
+  box-sizing: border-box; }
+
+*, *::after, *::before {
+  box-sizing: inherit; }
+
+body {
+  background: white; }
+  body p {
+    font-family: Helvetica, Arial, sans-serif;
+    color: white; }
+  body h3, body h4 {
+    font-family: Helvetica;
+    color: #8080ff; }
+
+.sidebar ul {
+  list-style-type: none; }
+
+a {
+  text-decoration: none;
+  font-family: Helvetica;
+  font-size: 20px;
+  color: white; }
+  a:hover {
+    color: gray; }
+
+div.grid-wrapper {
+  padding-top: 10px;
+  margin-bottom: 200px; }
+  div.grid-wrapper .teaserImage {
+    margin-top: 5px;
+    float: left;
+    display: block;
+    margin-right: 2.35765%;
+    width: 23.23176%;
+    background-color: white;
+    text-align: center; }
+    div.grid-wrapper .teaserImage:last-child {
+      margin-right: 0; }
+    @media screen and (max-width: 768px) {
+      div.grid-wrapper .teaserImage {
+        float: left;
+        display: block;
+        margin-right: 4.82916%;
+        width: 30.11389%; }
+        div.grid-wrapper .teaserImage:last-child {
+          margin-right: 0; } }
+  div.grid-wrapper .teaserText {
+    margin-top: 5px;
+    float: left;
+    display: block;
+    margin-right: 2.35765%;
+    width: 57.35098%;
+    background-color: black; }
+    div.grid-wrapper .teaserText:last-child {
+      margin-right: 0; }
+    @media screen and (max-width: 768px) {
+      div.grid-wrapper .teaserText {
+        float: left;
+        display: block;
+        margin-right: 4.82916%;
+        width: 65.05695%; }
+        div.grid-wrapper .teaserText:last-child {
+          margin-right: 0; } }
+  div.grid-wrapper .sidebar {
+    float: left;
+    display: block;
+    margin-right: 2.35765%;
+    width: 14.70196%;
+    background-color: black;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+    margin-top: 10px;
+    float: right; }
+    div.grid-wrapper .sidebar:last-child {
+      margin-right: 0; }
+    div.grid-wrapper .sidebar a {
+      font-size: 20px; }
+    div.grid-wrapper .sidebar li {
+      line-height: 150%; }
+    div.grid-wrapper .sidebar h4 {
+      margin-left: 10px; }
+    @media screen and (max-width: 768px) {
+      div.grid-wrapper .sidebar {
+        float: left;
+        display: block;
+        margin-right: 4.82916%;
+        width: 100%; }
+        div.grid-wrapper .sidebar:last-child {
+          margin-right: 0; } }
+
+header.grid-wrapper, footer.grid-wrapper {
+  float: left;
+  display: block;
+  margin-right: 2.35765%;
+  width: 100%;
+  background-color: black;
+  -webkit-border-radius: 10px;
+  -moz-border-radius: 10px;
+  border-radius: 10px; }
+  header.grid-wrapper:last-child, footer.grid-wrapper:last-child {
+    margin-right: 0; }
+  @media screen and (max-width: 768px) {
+    header.grid-wrapper, footer.grid-wrapper {
+      float: left;
+      display: block;
+      margin-right: 4.82916%;
+      width: 100%; }
+      header.grid-wrapper:last-child, footer.grid-wrapper:last-child {
+        margin-right: 0; } }
+
+footer {
+  margin-top: 20px; }
+
+footer h2, footer p, header h2, header p {
+  float: left;
+  color: #8080ff;
+  margin-left: 10px; }
+
+footer ul, header ul {
+  float: right;
+  margin-right: 40px;
+  list-style-type: none; }
+
+footer ul li, header ul li {
+  float: left; }
+
+footer p, header p {
+  margin-top: 25px; }
+
+/*# sourceMappingURL=app.cs.map */
